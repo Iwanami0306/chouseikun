e3fcb3a80c958be30281190ca4667e0c358bcf7b
Haruka Iwanami <s1711432@s.tsukuba.ac.jp>
2019-06-14 12:37:14 +0900

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   
+  get '/login', to:'sessions#new'
+
+  post '/login', to:'sessions#create'
+
+  delete '/logout', to:'sessions#destroy'
+
   post '/event_create', to:'events#create'
   
   get '/event_create', to:'events#new'
